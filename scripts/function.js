54cadcce31d5199929eaa3bfef3c0ab9cf1ecf86
goze <goze.qiu@gmail.com>
2017-08-04 12:45:45 +0800

    fix: update to func

--- scripts/function.js
+++ scripts/function.js
@@ -25,41 +25,45 @@
 // const result = arrToJson(['a','b','c','d'])
 // console.log(result); // {"a":{"b":{"c":"d"}}}
 
-
 /**
  * to use Object.assign()
  */
-let count = 0;
-const arrToJson = (arr) => {
+function arrToJsonNull(arr) {
+  let count = 0;
+  const arrToJson = (arr) => {
 
-  let num = arr.length - 1;
+    let num = arr.length - 1;
 
-  if (count === 0) {
-    count++;
-    let tmp = {};
-    tmp[arr[arr.length - 1]] = null;
+    if (count === 0) {
+      count++;
+      let tmp = {};
+      tmp[arr[arr.length - 1]] = null;
 
-    if (arr.length > 1) {
-      arr.pop();
-      arr.push(tmp);
-      return arrToJson(arr);
+      if (arr.length > 1) {
+        arr.pop();
+        arr.push(tmp);
+        return arrToJson(arr);
+      }
+      return arrToJson([tmp]);
     }
-    return arrToJson([tmp]);
+
+    if (num === 0) {
+      return JSON.stringify(arr[0]);
+    }
+
+    let tmp = {}
+    tmp[arr[num - 1]] = arr[num];
+    arr = arr.slice(0, -2);
+    arr.push(tmp);
+
+    count++;
+
+    return arrToJson(arr);
   }
-
-  if (num === 0) {
-    return JSON.stringify(arr[0]);
-  }
-
-  let tmp = {}
-  tmp[arr[num - 1]] = arr[num];
-  arr = arr.slice(0, -2);
-  arr.push(tmp);
-
-  count++;
 
   return arrToJson(arr);
 }
 
-// console.log(arrToJson(['a', 'b', 'c'])) // {"a":{"b":{"c":null}}}
-console.log(arrToJson(['a'])) // {"a":null}
+
+console.log(arrToJsonNull(['a', 'b', 'c'])) // {"a":{"b":{"c":null}}}
+// console.log(arrToJsonNull(['a'])) // {"a":null}
