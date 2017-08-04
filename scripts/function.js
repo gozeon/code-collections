ce5c4fe5318c17aaf013bfdc2f57d2c304fd9991
goze <goze.qiu@gmail.com>
2017-08-04 12:46:12 +0800

    refactor: yes

--- scripts/function.js
+++ scripts/function.js
@@ -10,20 +10,20 @@
  * }
  * @param {Array} arr 
  */
-// const arrToJson = (arr) => {
-//   let num = arr.length - 1;
-//   if (num === 0) {
-//     return JSON.stringify(arr[0]);
-//   }
-//   let tmp = {};
-//   tmp[arr[num - 1]] = arr[num];
-//   arr = arr.slice(0, -2);
-//   arr.push(tmp);
-//   return arrToJson(arr);
-// }
+const arrToJson = (arr) => {
+  let num = arr.length - 1;
+  if (num === 0) {
+    return JSON.stringify(arr[0]);
+  }
+  let tmp = {};
+  tmp[arr[num - 1]] = arr[num];
+  arr = arr.slice(0, -2);
+  arr.push(tmp);
+  return arrToJson(arr);
+}
 
-// const result = arrToJson(['a','b','c','d'])
-// console.log(result); // {"a":{"b":{"c":"d"}}}
+const result = arrToJson(['a','b','c','d'])
+console.log(result); // {"a":{"b":{"c":"d"}}}
 
 /**
  * to use Object.assign()
