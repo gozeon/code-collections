761bbe30c09c6c825b78a8b5dee47437c9a5a0fd
goze <goze.qiu@gmail.com>
2019-03-06 17:30:05 +0800

    doc: reference

--- python/rabbitmq/readme.md
+++ python/rabbitmq/readme.md
@@ -1,1 +1,7 @@
 http://brunorocha.org/python/microservices-with-python-rabbitmq-and-nameko.html
+
+
+https://blog.csdn.net/LeiXiaoTao_Java/article/details/78909760
+
+
+https://youtu.be/6EYHWRSi1DM
--- python/rabbitmq/service.py
+++ python/rabbitmq/service.py
@@ -3,39 +3,39 @@ from nameko.rpc import rpc, RpcProxy
 
 
 class Mail(object):
-    name = "mail"
+	name = "mail"
 
-    @rpc
-    def send(self, to, subject, contents):
-        yag = yagmail.SMTP(user='liqiushuai@126.com',
-                           password='qiankun.230',
-                           host='smtp.126.com',
-                           port=587)
-        # yag.send(to=to.encode('utf-8'),
-        #          subject=subject.encode('utf-8'),
-        #          contents=[contents.encode('utf8')])
-        yag.send(to=to, subject=subject, contents=[contents])
+	@rpc
+	def send(self, to, subject, contents):
+		yag = yagmail.SMTP(user='xxxx',
+						   password='xxx',
+						   host='smtp.126.com',
+						   port=587)
+		# yag.send(to=to.encode('utf-8'),
+		#          subject=subject.encode('utf-8'),
+		#          contents=[contents.encode('utf8')])
+		yag.send(to=to, subject=subject, contents=[contents])
 
 
 class Compute(object):
-    name = "compute"
-    mail = RpcProxy('mail')
+	name = "compute"
+	mail = RpcProxy('mail')
 
-    @rpc
-    def compute(self, operation, value, other, email):
-        operations = {'sum': lambda x, y: int(x) + int(y),
-                      'mul': lambda x, y: int(x) * int(y),
-                      'div': lambda x, y: int(x) / int(y),
-                      'sub': lambda x, y: int(x) - int(y)}
-        try:
-            result = operations[operation](value, other)
-        except Exception as e:
-            self.mail.send.call_async(email, "An error occurred", str(e))
-            raise
-        else:
-            self.mail.send.call_async(
-                email,
-                "Your operation is complete!",
-                "The result is: %s" % result
-            )
-            return result
+	@rpc
+	def compute(self, operation, value, other, email):
+		operations = {'sum': lambda x, y: int(x) + int(y),
+					  'mul': lambda x, y: int(x) * int(y),
+					  'div': lambda x, y: int(x) / int(y),
+					  'sub': lambda x, y: int(x) - int(y)}
+		try:
+			result = operations[operation](value, other)
+		except Exception as e:
+			self.mail.send.call_async(email, "An error occurred", str(e))
+			raise
+		else:
+			self.mail.send.call_async(
+				email,
+				"Your operation is complete!",
+				"The result is: %s" % result
+			)
+			return result
