9fe4e3829a5d01b78d078710d40f5d85add195bd
goze <goze.qiu@gmail.com>
2019-09-06 02:08:56 +0800

    vue event bus

---
+++ vue-eventbus/src/views/Bus.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <rightside />
+    <leftside />
+  </div>
+</template>
+
+<script>
+import Rightside from '@/components/RightSide.vue'
+import Leftside from '@/components/LeftSide.vue'
+
+
+export default {
+  name: 'bus',
+  components: {
+    Rightside,
+    Leftside
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
