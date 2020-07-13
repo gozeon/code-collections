11c4ca420ceaa11d23f65d6702e38bc44550c2a3
goze <goze.qiu@gmail.com>
2020-07-13 19:46:27 +0800

    xgplayer parcel demo

---
+++ xgplayer-test/main.js
@@ -0,0 +1,9 @@
+import Player from 'xgplayer';
+import './.xgplayer/skin/index.js'
+
+
+let player = new Player({
+	id: 'mse',
+	url: '//s1.pstatp.com/cdn/expire-1-M/byted-player-videos/1.0.0/xgplayer-demo.mp4',
+	poster: '//s2.pstatp.com/cdn/expire-1-M/byted-player-videos/1.0.0/poster.jpg'
+});
